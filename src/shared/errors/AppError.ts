35e13b169f803817dd0d26e87ce9f122e3fbac27
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2020-11-06 23:26:16 -0300

    chore: create server

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,12 @@
+//exception handling
+
+export default class AppError {
+  public readonly message: string;
+
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode: number = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import express, { Request, Response, NextFunction } from 'express';
+import routes from './routes/index';
+import cors from 'cors';
+import AppError from '@shared/errors/AppError';
+import '@shared/infra/typeorm';
+import '@shared/container';
+
+const app = express();
+const port = process.env.port || 3333;
+
+app.use(routes);
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  console.log(err);
+
+  return res.status(500).json(
+    {
+      error: '500',
+      message: 'internal server error',
+    },
+  );
+
+});
+
+app.listen(port, () => {
+  console.log(` Server Running at ${port}`);
+});
